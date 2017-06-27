0d51f4e91603908f5240d20c31529a436418375f
Moritz Schilling <moritz.schilling@dfki.de>
2017-06-27 09:54:22 +0200

    Added implementation encoding to software graph
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

--- include/SoftwareGraph.hpp
+++ include/SoftwareGraph.hpp
@@ -11,17 +11,36 @@ namespace Software {
     In this environment there are two main entities, concepts & relations between them.
     This class is derived from the concept graph class.
 
-    It introduces these different concepts: ALGORITHM, INPUT, OUTPUT, INTERFACE
+    It introduces these different concepts:
+        ALGORITHM, INPUT, OUTPUT, INTERFACE (for logical specification and skeleton generation)
+        IMPLEMENTATION, DATATYPE, LANGUAGE (for storing already available implementations of algorithms in different languages)
     The domain is encoded as follows:
     
     ALGORITHM -- has --> INTERFACE
-    INPUT <-- is-a --> INTERFACE
-    OUTPUT <-- is-a --> INTERFACE
-    ALGORITHM <-- needs --> INPUT
-    ALGORITHM <-- provides --> OUTPUT
-    INPUT <-- dependsOn --> OUTPUT
+    INPUT -- is-a --> INTERFACE
+    OUTPUT -- is-a --> INTERFACE
+    ALGORITHM -- needs --> INPUT
+    ALGORITHM -- provides --> OUTPUT
+    INPUT -- dependsOn --> OUTPUT
+
+    ALGORITHM <-- realizes -- IMPLEMENTATION -- expressedIn --> LANGUAGE
+    INTERFACE <-- realizes -- DATATYPE -- expressedIn --> LANGUAGE
+    IMPLEMENTATION -- uses --> DATATYPE
+    (IMPLEMENTATION -- dependsOn --> IMPLEMENTATION)
+    (LANGUAGE -- has --> COMPILER)
 
     If some X is a ALGORITHM then there exists a path of IS-A relations from X to ALGORITHM
+
+    Some example:
+    
+        |---------- realizes -- disparity.c -- expressedIn --> C
+        v
+    DisparityMap -- needs --> left -- is-a --> Input
+      | |                       |---- is-a --> Image <-- realizes -- uint8_t[MAX_X][MAX_Y] -- expressedIn --> C
+      | |---------- needs --> right ...
+      |------------ provides --> disparity ...
+
+    NOTE: When merging with the concept of Finite State Machines, this whole thing would become an even whiter box :)
     
     Open questions:
     * Does it makes sense to have 1-1, 1-N, N-1 and N-M versions of the relations?
@@ -49,12 +68,18 @@ class Graph : public Conceptgraph
         static const unsigned InterfaceId;
         static const unsigned InputId;
         static const unsigned OutputId;
+        static const unsigned ImplementationId;
+        static const unsigned LanguageId;
+        static const unsigned DatatypeId;
         // Ids for identifiing IsA, HasA and Connects
         static const unsigned IsAId;
         static const unsigned HasAId;
         static const unsigned NeedsId;
         static const unsigned ProvidesId;
         static const unsigned DependsOnId;
+        static const unsigned ExpressedInId;
+        static const unsigned RealizesId;
+        static const unsigned UsesId;
 
         // Constructor/Destructor
         Graph();
@@ -69,13 +94,18 @@ class Graph : public Conceptgraph
         unsigned createInterface(const std::string& name="Interface");
         unsigned createInput(const unsigned interfaceId, const std::string& name="Input");
         unsigned createOutput(const unsigned interfaceId, const std::string& name="Output");
+        unsigned createImplementation(const std::string& name="Implementation");
+        unsigned createDatatype(const std::string& name="DataType");
+        unsigned createLanguage(const std::string& name="Language");
 
         // Queries
-        // NOTE: Return true sets whose members are all of the same type/superclass
         Hyperedges algorithms(const std::string& name="");
         Hyperedges interfaces(const std::string& name="");
         Hyperedges inputs(const std::string& name="");
         Hyperedges outputs(const std::string& name="");
+        Hyperedges implementations(const std::string& name="");
+        Hyperedges datatypes(const std::string& name="");
+        Hyperedges languages(const std::string& name="");
     
         // Algorithms & I/O/P
         // RULE: A has X -> X is-a Interface
@@ -84,10 +114,15 @@ class Graph : public Conceptgraph
         unsigned has(const unsigned algorithmId, const unsigned interfaceId);
         unsigned provides(const unsigned algorithmId, const unsigned outputId);
         unsigned needs(const unsigned algorithmId, const unsigned inputId);
-
         // I/O & Dependencies
         // RULE: I dependsOn O -> I is-a Input, O is-a Output
         unsigned depends(const unsigned inputId, const unsigned outputId);
+        // Algorithm/Interface & Implementation/Datatype
+        unsigned realizes(const unsigned implementationOrDatatypeId, const unsigned algorithmOrInterfaceId);
+        // Implementation/Datatype & Language
+        unsigned expressedIn(const unsigned implementationOrDatatypeId, const unsigned languageId);
+        // Implementation & Datatype
+        unsigned uses(const unsigned implementationId, const unsigned datatypeId);
 };
 
 }
--- src/SoftwareGraph.cpp
+++ src/SoftwareGraph.cpp
@@ -8,12 +8,18 @@ const unsigned Graph::InterfaceId = 2001;
 const unsigned Graph::InputId = 2002;
 const unsigned Graph::OutputId = 2003;
 const unsigned Graph::AlgorithmId = 2000;
+const unsigned Graph::ImplementationId = 2004;
+const unsigned Graph::DatatypeId = 2005;
+const unsigned Graph::LanguageId = 2006;
 // Relation Ids
-const unsigned Graph::IsAId      = 111;
-const unsigned Graph::HasAId     = 222;
-const unsigned Graph::DependsOnId = 444;
-const unsigned Graph::NeedsId = 555;
-const unsigned Graph::ProvidesId = 666;
+const unsigned Graph::IsAId       = 11;
+const unsigned Graph::HasAId      = 22;
+const unsigned Graph::DependsOnId = 44;
+const unsigned Graph::NeedsId     = 55;
+const unsigned Graph::ProvidesId  = 66;
+const unsigned Graph::ExpressedInId = 77;
+const unsigned Graph::RealizesId  = 88;
+const unsigned Graph::UsesId      = 99;
 
 // GRAPH STUFF
 void Graph::createMainConcepts()
@@ -22,12 +28,21 @@ void Graph::createMainConcepts()
     Conceptgraph::create(Graph::InterfaceId, "INTERFACE");
     Conceptgraph::create(Graph::InputId, "INPUT");
     Conceptgraph::create(Graph::OutputId, "OUTPUT");
+    Conceptgraph::create(Graph::ImplementationId, "IMPLEMENTATION");
+    Conceptgraph::create(Graph::DatatypeId, "DATATYPE");
+    Conceptgraph::create(Graph::LanguageId, "LANGUAGE");
     // FIXME: The following concepts should be relations not concepts ... or?
+    //Conceptgraph::defineRelation(Graph::HasAId, "IS-A", Graph::AlgorithmId, Graph::InterfaceId);
+    //When stating a fact, we should do this (it will also instantiate from MASTER HAS-A)
+    //Conceptgraph::relate(algorithmId, interfaceId, Graph::HasAId);
     Conceptgraph::create(Graph::IsAId, "IS-A");
     Conceptgraph::create(Graph::HasAId, "HAS-A");
     Conceptgraph::create(Graph::DependsOnId, "DEPENDS-ON");
     Conceptgraph::create(Graph::NeedsId, "NEEDS");
     Conceptgraph::create(Graph::ProvidesId, "PROVIDES");
+    Conceptgraph::create(Graph::ExpressedInId, "EXPRESSED-IN");
+    Conceptgraph::create(Graph::RealizesId, "REALIZES");
+    Conceptgraph::create(Graph::UsesId, "USES");
 }
 
 Graph::Graph()
@@ -77,6 +92,30 @@ Hypergraph::Hyperedges Graph::outputs(const std::string& name)
     return result;
 }
 
+Hypergraph::Hyperedges Graph::implementations(const std::string& name)
+{
+    Hyperedges result;
+    result = Conceptgraph::traverse(Graph::ImplementationId, name, get(Graph::IsAId)->label(), UP);
+    result.erase(Graph::ImplementationId);
+    return result;
+}
+
+Hypergraph::Hyperedges Graph::datatypes(const std::string& name)
+{
+    Hyperedges result;
+    result = Conceptgraph::traverse(Graph::DatatypeId, name, get(Graph::IsAId)->label(), UP);
+    result.erase(Graph::DatatypeId);
+    return result;
+}
+
+Hypergraph::Hyperedges Graph::languages(const std::string& name)
+{
+    Hyperedges result;
+    result = Conceptgraph::traverse(Graph::LanguageId, name, get(Graph::IsAId)->label(), UP);
+    result.erase(Graph::LanguageId);
+    return result;
+}
+
 unsigned Graph::createAlgorithm(const std::string& name)
 {
     createMainConcepts();
@@ -98,7 +137,7 @@ unsigned Graph::createOutput(const unsigned interfaceId, const std::string& name
 {
     createMainConcepts();
     unsigned a = create(name);
-    relate(a, Graph::OutputId, get(Graph::IsAId)->label()); // TODO: This should be in a base class
+    relate(a, Graph::OutputId, get(Graph::IsAId)->label());
     relate(a, interfaceId, get(Graph::IsAId)->label());
     return a;
 }
@@ -107,7 +146,31 @@ unsigned Graph::createInterface(const std::string& name)
 {
     createMainConcepts();
     unsigned a = create(name);
-    relate(a, Graph::InterfaceId, get(Graph::IsAId)->label()); // TODO: This should be in a base class
+    relate(a, Graph::InterfaceId, get(Graph::IsAId)->label());
+    return a;
+}
+
+unsigned Graph::createImplementation(const std::string& name)
+{
+    createMainConcepts();
+    unsigned a = create(name);
+    relate(a, Graph::ImplementationId, get(Graph::IsAId)->label());
+    return a;
+}
+
+unsigned Graph::createDatatype(const std::string& name)
+{
+    createMainConcepts();
+    unsigned a = create(name);
+    relate(a, Graph::DatatypeId, get(Graph::IsAId)->label());
+    return a;
+}
+
+unsigned Graph::createLanguage(const std::string& name)
+{
+    createMainConcepts();
+    unsigned a = create(name);
+    relate(a, Graph::LanguageId, get(Graph::IsAId)->label());
     return a;
 }
 
@@ -143,6 +206,34 @@ unsigned Graph::depends(const unsigned inputId, const unsigned outputId)
     if (inputs().count(inputId) && outputs().count(outputId))
     {
         return relate(inputId, outputId, get(Graph::DependsOnId)->label());
+    }
+    return 0;
+}
+
+unsigned Graph::realizes(const unsigned implementationOrDatatypeId, const unsigned algorithmOrInterfaceId)
+{
+    if ((implementations().count(implementationOrDatatypeId) && algorithms().count(algorithmOrInterfaceId)) ||
+        (datatypes().count(implementationOrDatatypeId) && interfaces().count(algorithmOrInterfaceId)))
+    {
+        return relate(implementationOrDatatypeId, algorithmOrInterfaceId, get(Graph::RealizesId)->label());
+    }
+    return 0;
+}
+
+unsigned Graph::expressedIn(const unsigned implementationOrDatatypeId, const unsigned languageId)
+{
+    if ((implementations().count(implementationOrDatatypeId) || datatypes().count(implementationOrDatatypeId)) && languages().count(languageId))
+    {
+        return relate(implementationOrDatatypeId, languageId, get(Graph::ExpressedInId)->label());
+    }
+    return 0;
+}
+
+unsigned Graph::uses(const unsigned implementationId, const unsigned datatypeId)
+{
+    if (implementations().count(implementationId) && datatypes().count(datatypeId))
+    {
+        return relate(implementationId, datatypeId, get(Graph::UsesId)->label());
     }
     return 0;
 }
