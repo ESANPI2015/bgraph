2bcc2b60db230bc6e3883556319051dd614902c2
Moritz Schilling <moritz.schilling@dfki.de>
2017-09-13 09:48:54 +0200

    Test deep instantiation & composition
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

--- test/testBG.cpp
+++ test/testBG.cpp
@@ -11,46 +11,22 @@ int main(void)
     CommonConceptGraph ccgraph(cgraph);
     Software::Graph swgraph(ccgraph);
 
-    // Lets instantiate a small network
-    // First we say, that we want a algorithm SUM connected to algorithm DIV
     auto sumClass = swgraph.algorithmClasses("SUM");
     auto divClass = swgraph.algorithmClasses("DIVIDE");
-    std::cout << "#SUM: " << sumClass.size() << " #DIV: " << divClass.size() << "\n";
-    // Get also all inputs & outputs
-    auto sumClassOutputs = intersect(swgraph.childrenOf(sumClass), swgraph.outputs());
-    auto divClassInputs = intersect(swgraph.childrenOf(divClass), swgraph.inputs());
-    std::cout << "#SUM Out: " << sumClassOutputs.size() << " #DIV In: " << divClassInputs.size() << "\n";
 
-    // Now, instantiate a SUM and a DIV
-    auto sum = swgraph.instantiateFrom(sumClass);
-    auto div = swgraph.instantiateFrom(divClass);
+    // Deep instantiation of a sum and a div algorithm
+    auto sum = swgraph.instantiateDeepFrom(sumClass);
+    auto div = swgraph.instantiateDeepFrom(divClass);
 
-    // TODO: Test if we have now both in algorithms() result!!!
-    std::cout << "Algorithms: " << swgraph.algorithms() << "\n";
+    std::cout << "Children of DIV: " << swgraph.childrenOf(div) << "\n";
 
-    // For each of the inputs/outputs: find superclass and instantiate from that
-    Hyperedges sumOutputs;
-    for (auto sumClassOutputId : sumClassOutputs)
-    {
-        std::string label = swgraph.get(sumClassOutputId)->label();
-        // instantiate also all sumClassOutputs and connect them to sum instance
-        auto sumClassOutputClass = swgraph.instancesOf(sumClassOutputId,"", CommonConceptGraph::TraversalDirection::DOWN);
-        sumOutputs = unite(sumOutputs, swgraph.instantiateFrom(sumClassOutputClass, label));
-    }
-    swgraph.provides(sum, sumOutputs);
-    // the same with div but now only inputs
-    Hyperedges divInputs;
-    for (auto divClassInputId : divClassInputs)
-    {
-        std::string label = swgraph.get(divClassInputId)->label();
-        // instantiate also all divClassInputs and connect them to sum instance
-        auto divClassInputClass = swgraph.instancesOf(divClassInputId,"", CommonConceptGraph::TraversalDirection::DOWN);
-        divInputs = unite(divInputs, swgraph.instantiateFrom(divClassInputClass, label));
-    }
-    swgraph.needs(div, divInputs);
+    // Conncet the merged output of sum with the x input of div
+    swgraph.depends(swgraph.childrenOf(div,"x"), swgraph.childrenOf(sum,"merged"));
 
-    // Finally, we create the network!
-    swgraph.depends(divInputs, sumOutputs);
+    // Create a new class of algorithm which is a subtype of SUBGRAPH
+    swgraph.isA(swgraph.createAlgorithm("myFirstSubgraph"), swgraph.algorithmClasses("SUBGRAPH"));
+    swgraph.partOf(sum, swgraph.algorithmClasses("myFirstSubgraph"));
+    swgraph.partOf(div, swgraph.algorithmClasses("myFirstSubgraph"));
 
     // Store the graph
     YAML::Node test;
