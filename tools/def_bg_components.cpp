df6ba1edc766d6b2de57e5ff7e7d3c49b60a58f3
Moritz Schilling <moritz.schilling@dfki.de>
2017-09-29 11:05:53 +0200

    Better definition of the behavior graph language
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

--- tools/def_bg_components.cpp
+++ tools/def_bg_components.cpp
@@ -10,115 +10,131 @@ int main(void)
     Software::Graph swgraph;
 
     // Some constants
+    //const unsigned int maxMergeInputs = 256;
     const unsigned int maxMergeInputs = 10;
+    //const unsigned int maxInputPorts = 512;
 
     // Supported Languages
     auto cId = swgraph.instantiateFrom(swgraph.createLanguage("C"));
     auto vhdlId = swgraph.instantiateFrom(swgraph.createLanguage("VHDL"));
 
     // Only interface: reals
-    // TODO: Shouldn't these be instances?
     auto realId = swgraph.createInterface("RealNumber");
-    auto realVecId = swgraph.createInterface("RealNumberVec");
-    auto numOfEntriesId = swgraph.createInterface("RealNumberVecEntries");
+
     // In C: float32
     // TODO: Shouldn't these be instances?
     auto floatId = swgraph.createDatatype("float");
-    auto floatVecId = swgraph.createDatatype("float[10]");
     auto uintId = swgraph.createDatatype("uint8_t");
+
     // In VHDL: std_logic_vector(31 downto 0)
     // TODO: Shouldn't these be instances?
     auto stdvecId = swgraph.createDatatype("std_logic_vector(31 downto 0)");
-    auto stdvecVecId = swgraph.createDatatype("array (0 to 9) of std_logic_vector(31 downto 0)");
-    auto uintvecId = swgraph.createDatatype("std_logic_vector(7 downto 0)");
     swgraph.represents(unite(floatId, stdvecId), realId);
-    swgraph.represents(unite(floatVecId, stdvecVecId), realVecId);
-    swgraph.represents(unite(uintId, uintvecId), numOfEntriesId);
-    swgraph.expressedIn(unite(floatId, unite(floatVecId, uintId)), cId);
-    swgraph.expressedIn(unite(stdvecId, unite(stdvecVecId, uintvecId)), vhdlId);
+    swgraph.expressedIn(floatId, cId);
+    swgraph.expressedIn(stdvecId, vhdlId);
 
     // Input/Output classes
     auto inputClassId = swgraph.createInput(realId, "RealInput");
-    auto mergeClassId = swgraph.createInput(realVecId, "RealVecInput");
-    auto entriesClassId = swgraph.createInput(numOfEntriesId, "VecEntries");
     auto outputClassId = swgraph.createOutput(realId, "RealOutput");
 
+    // Upper classes:
+    // NODE class represents the primitive nodes of the behavior graph domain
+    // MERGE class represents the merge functions which preprocess the incoming data before it is used by nodes
+    // EDGE class represents the computations performed by edges - currently this should be a simple multiplication
+    auto nodeClassId = swgraph.createAlgorithm("NODE");
+    auto mergeClassId = swgraph.createAlgorithm("MERGE");
+    auto edgeClassId = swgraph.createAlgorithm("EDGE");
+
+    // The NODE class
     // Build up the behavior graph components aka Nodes
     // 1-to-1
     Hyperedges id1, id2;
     auto pipeId = swgraph.createAlgorithm("PIPE");
+    swgraph.isA(pipeId, nodeClassId);
     id1 = swgraph.instantiateInput(inputClassId, "x");
     id2 = swgraph.instantiateOutput(outputClassId, "y");
     swgraph.needs(pipeId, id1);
     swgraph.provides(pipeId, id2);
 
     auto divId = swgraph.createAlgorithm("DIVIDE");
+    swgraph.isA(divId, nodeClassId);
     id1 = swgraph.instantiateInput(inputClassId, "x");
     id2 = swgraph.instantiateOutput(outputClassId, "y");
     swgraph.needs(divId, id1);
     swgraph.provides(divId, id2);
 
     auto sinId = swgraph.createAlgorithm("SIN");
+    swgraph.isA(sinId, nodeClassId);
     id1 = swgraph.instantiateInput(inputClassId, "x");
     id2 = swgraph.instantiateOutput(outputClassId, "y");
     swgraph.needs(sinId, id1);
     swgraph.provides(sinId, id2);
 
     auto cosId = swgraph.createAlgorithm("COS");
+    swgraph.isA(cosId, nodeClassId);
     id1 = swgraph.instantiateInput(inputClassId, "x");
     id2 = swgraph.instantiateOutput(outputClassId, "y");
     swgraph.needs(cosId, id1);
     swgraph.provides(cosId, id2);
 
     auto tanId = swgraph.createAlgorithm("TAN");
+    swgraph.isA(tanId, nodeClassId);
     id1 = swgraph.instantiateInput(inputClassId, "x");
     id2 = swgraph.instantiateOutput(outputClassId, "y");
     swgraph.needs(tanId, id1);
     swgraph.provides(tanId, id2);
 
     auto tanhId = swgraph.createAlgorithm("TANH");
+    swgraph.isA(tanhId, nodeClassId);
     id1 = swgraph.instantiateInput(inputClassId, "x");
     id2 = swgraph.instantiateOutput(outputClassId, "y");
     swgraph.needs(tanhId, id1);
     swgraph.provides(tanhId, id2);
 
     auto acosId = swgraph.createAlgorithm("ACOS");
+    swgraph.isA(acosId, nodeClassId);
     id1 = swgraph.instantiateInput(inputClassId, "x");
     id2 = swgraph.instantiateOutput(outputClassId, "y");
     swgraph.needs(acosId, id1);
     swgraph.provides(acosId, id2);
 
     auto asinId = swgraph.createAlgorithm("ASIN");
+    swgraph.isA(asinId, nodeClassId);
     id1 = swgraph.instantiateInput(inputClassId, "x");
     id2 = swgraph.instantiateOutput(outputClassId, "y");
     swgraph.needs(asinId, id1);
     swgraph.provides(asinId, id2);
 
     auto atanId = swgraph.createAlgorithm("ATAN");
+    swgraph.isA(atanId, nodeClassId);
     id1 = swgraph.instantiateInput(inputClassId, "x");
     id2 = swgraph.instantiateOutput(outputClassId, "y");
     swgraph.needs(atanId, id1);
     swgraph.provides(atanId, id2);
 
     auto logId = swgraph.createAlgorithm("LOG");
+    swgraph.isA(logId, nodeClassId);
     id1 = swgraph.instantiateInput(inputClassId, "x");
     id2 = swgraph.instantiateOutput(outputClassId, "y");
     swgraph.needs(logId, id1);
     swgraph.provides(logId, id2);
 
     auto expId = swgraph.createAlgorithm("EXP");
+    swgraph.isA(expId, nodeClassId);
     id1 = swgraph.instantiateInput(inputClassId, "x");
     id2 = swgraph.instantiateOutput(outputClassId, "y");
     swgraph.needs(expId, id1);
     swgraph.provides(expId, id2);
 
     auto absId = swgraph.createAlgorithm("ABS");
+    swgraph.isA(absId, nodeClassId);
     id1 = swgraph.instantiateInput(inputClassId, "x");
     id2 = swgraph.instantiateOutput(outputClassId, "y");
     swgraph.needs(absId, id1);
     swgraph.provides(absId, id2);
 
     auto sqrtId = swgraph.createAlgorithm("SQRT");
+    swgraph.isA(sqrtId, nodeClassId);
     id1 = swgraph.instantiateInput(inputClassId, "x");
     id2 = swgraph.instantiateOutput(outputClassId, "y");
     swgraph.needs(sqrtId, id1);
@@ -127,6 +143,7 @@ int main(void)
     // 2-to-1
     Hyperedges id3;
     auto atan2Id = swgraph.createAlgorithm("ATAN2");
+    swgraph.isA(atan2Id, nodeClassId);
     id1 = swgraph.instantiateInput(inputClassId, "x");
     id2 = swgraph.instantiateInput(inputClassId, "y");
     id3 = swgraph.instantiateOutput(outputClassId, "z");
@@ -134,6 +151,7 @@ int main(void)
     swgraph.provides(atan2Id, id3);
 
     auto powId = swgraph.createAlgorithm("POW");
+    swgraph.isA(powId, nodeClassId);
     id1 = swgraph.instantiateInput(inputClassId, "x");
     id2 = swgraph.instantiateInput(inputClassId, "y");
     id3 = swgraph.instantiateOutput(outputClassId, "z");
@@ -141,6 +159,7 @@ int main(void)
     swgraph.provides(powId, id3);
 
     auto modId = swgraph.createAlgorithm("MOD");
+    swgraph.isA(modId, nodeClassId);
     id1 = swgraph.instantiateInput(inputClassId, "x");
     id2 = swgraph.instantiateInput(inputClassId, "y");
     id3 = swgraph.instantiateOutput(outputClassId, "z");
@@ -150,6 +169,7 @@ int main(void)
     // 3-to-1
     Hyperedges id4;
     auto greaterId = swgraph.createAlgorithm(">0");
+    swgraph.isA(greaterId, nodeClassId);
     id1 = swgraph.instantiateInput(inputClassId, "condition");
     id2 = swgraph.instantiateInput(inputClassId, "true");
     id3 = swgraph.instantiateInput(inputClassId, "false");
@@ -158,6 +178,7 @@ int main(void)
     swgraph.provides(greaterId, id4);
 
     auto equalId = swgraph.createAlgorithm("==0");
+    swgraph.isA(equalId, nodeClassId);
     id1 = swgraph.instantiateInput(inputClassId, "condition");
     id2 = swgraph.instantiateInput(inputClassId, "true");
     id3 = swgraph.instantiateInput(inputClassId, "false");
@@ -165,82 +186,107 @@ int main(void)
     swgraph.needs(equalId, unite(unite(id1, id2), id3));
     swgraph.provides(equalId, id4);
 
+    // The MERGE class
     // (UP-TO-N)-to-1 aka Merges
     // they also have "bias" and "defaultValue"
     Hyperedges inputIds;
     auto sumId = swgraph.createAlgorithm("SUM");
+    swgraph.isA(sumId, mergeClassId);
     inputIds=unite(inputIds,swgraph.instantiateInput(inputClassId, "defaultValue"));
     inputIds=unite(inputIds,swgraph.instantiateInput(inputClassId, "bias"));
-    inputIds=unite(inputIds, swgraph.instantiateInput(mergeClassId, "values"));
-    inputIds=unite(inputIds, swgraph.instantiateInput(mergeClassId, "weights"));
-    inputIds=unite(inputIds, swgraph.instantiateInput(entriesClassId, "entries"));
+    for (unsigned i = 0; i < maxMergeInputs; ++i)
+    {
+        inputIds=unite(inputIds, swgraph.instantiateInput(inputClassId, std::to_string(i)));
+    }
     id1 = swgraph.instantiateOutput(outputClassId, "merged");
     swgraph.needs(sumId, inputIds);
     swgraph.provides(sumId, id1);
 
     inputIds.clear();
     auto prodId = swgraph.createAlgorithm("PRODUCT");
+    swgraph.isA(prodId, mergeClassId);
     inputIds=unite(inputIds,swgraph.instantiateInput(inputClassId, "defaultValue"));
     inputIds=unite(inputIds,swgraph.instantiateInput(inputClassId, "bias"));
-    inputIds=unite(inputIds, swgraph.instantiateInput(mergeClassId, "values"));
-    inputIds=unite(inputIds, swgraph.instantiateInput(mergeClassId, "weights"));
-    inputIds=unite(inputIds, swgraph.instantiateInput(entriesClassId, "entries"));
+    for (unsigned i = 0; i < maxMergeInputs; ++i)
+    {
+        inputIds=unite(inputIds, swgraph.instantiateInput(inputClassId, std::to_string(i)));
+    }
     id1 = swgraph.instantiateOutput(outputClassId, "merged");
     swgraph.needs(prodId, inputIds);
     swgraph.provides(prodId, id1);
 
     inputIds.clear();
     auto minId = swgraph.createAlgorithm("MIN");
+    swgraph.isA(minId, mergeClassId);
     inputIds=unite(inputIds,swgraph.instantiateInput(inputClassId, "defaultValue"));
     inputIds=unite(inputIds,swgraph.instantiateInput(inputClassId, "bias"));
-    inputIds=unite(inputIds, swgraph.instantiateInput(mergeClassId, "values"));
-    inputIds=unite(inputIds, swgraph.instantiateInput(mergeClassId, "weights"));
-    inputIds=unite(inputIds, swgraph.instantiateInput(entriesClassId, "entries"));
+    for (unsigned i = 0; i < maxMergeInputs; ++i)
+    {
+        inputIds=unite(inputIds, swgraph.instantiateInput(inputClassId, std::to_string(i)));
+    }
     id1 = swgraph.instantiateOutput(outputClassId, "merged");
     swgraph.needs(minId, inputIds);
     swgraph.provides(minId, id1);
 
     inputIds.clear();
     auto maxId = swgraph.createAlgorithm("MAX");
+    swgraph.isA(maxId, mergeClassId);
     inputIds=unite(inputIds,swgraph.instantiateInput(inputClassId, "defaultValue"));
     inputIds=unite(inputIds,swgraph.instantiateInput(inputClassId, "bias"));
-    inputIds=unite(inputIds, swgraph.instantiateInput(mergeClassId, "values"));
-    inputIds=unite(inputIds, swgraph.instantiateInput(mergeClassId, "weights"));
-    inputIds=unite(inputIds, swgraph.instantiateInput(entriesClassId, "entries"));
+    for (unsigned i = 0; i < maxMergeInputs; ++i)
+    {
+        inputIds=unite(inputIds, swgraph.instantiateInput(inputClassId, std::to_string(i)));
+    }
     id1 = swgraph.instantiateOutput(outputClassId, "merged");
     swgraph.needs(maxId, inputIds);
     swgraph.provides(maxId, id1);
 
     inputIds.clear();
     auto meanId = swgraph.createAlgorithm("MEAN");
+    swgraph.isA(meanId, mergeClassId);
     inputIds=unite(inputIds,swgraph.instantiateInput(inputClassId, "defaultValue"));
     inputIds=unite(inputIds,swgraph.instantiateInput(inputClassId, "bias"));
-    inputIds=unite(inputIds, swgraph.instantiateInput(mergeClassId, "values"));
-    inputIds=unite(inputIds, swgraph.instantiateInput(mergeClassId, "weights"));
-    inputIds=unite(inputIds, swgraph.instantiateInput(entriesClassId, "entries"));
+    for (unsigned i = 0; i < maxMergeInputs; ++i)
+    {
+        inputIds=unite(inputIds, swgraph.instantiateInput(inputClassId, std::to_string(i)));
+    }
     id1 = swgraph.instantiateOutput(outputClassId, "merged");
     swgraph.needs(meanId, inputIds);
     swgraph.provides(meanId, id1);
 
     inputIds.clear();
     auto normId = swgraph.createAlgorithm("NORM");
+    swgraph.isA(normId, mergeClassId);
     inputIds=unite(inputIds,swgraph.instantiateInput(inputClassId, "defaultValue"));
     inputIds=unite(inputIds,swgraph.instantiateInput(inputClassId, "bias"));
-    inputIds=unite(inputIds, swgraph.instantiateInput(mergeClassId, "values"));
-    inputIds=unite(inputIds, swgraph.instantiateInput(mergeClassId, "weights"));
-    inputIds=unite(inputIds, swgraph.instantiateInput(entriesClassId, "entries"));
+    for (unsigned i = 0; i < maxMergeInputs; ++i)
+    {
+        inputIds=unite(inputIds, swgraph.instantiateInput(inputClassId, std::to_string(i)));
+    }
     id1 = swgraph.instantiateOutput(outputClassId, "merged");
     swgraph.needs(normId, inputIds);
     swgraph.provides(normId, id1);
+
+    // The EDGE class
+    inputIds.clear();
+    auto edgeId = swgraph.createAlgorithm("EDGE");
+    swgraph.isA(edgeId, edgeClassId);
+    inputIds=unite(inputIds,swgraph.instantiateInput(inputClassId, "in"));
+    inputIds=unite(inputIds,swgraph.instantiateInput(inputClassId, "weight"));
+    id1 = swgraph.instantiateOutput(outputClassId, "out");
+    swgraph.needs(edgeId, inputIds);
+    swgraph.provides(edgeId, id1);
 
     // Special CLASSES
     // Extern behavior graph algorithms contain custom implementations
     // This means it is just a class with no interfaces on itself
     auto externClassId = swgraph.createAlgorithm("EXTERN");
+    swgraph.isA(externClassId, nodeClassId);
 
     // Subgraph behavior graph algorithms contain compositions of other behavior graph nodes
     // Again, this superclass has no interfaces on its own
     auto subgraphClassId = swgraph.createAlgorithm("SUBGRAPH");
+    swgraph.isA(subgraphClassId, nodeClassId);
 
     // Here is the end of the language definition
     // For the language to become usable, we now have to provide implementations for each of the atomic nodes for each of the languages
